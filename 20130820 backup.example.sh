786556b309528691b7dddedbc902ba4110a52985
belogrud <belogrud@gmail.com>
2025-03-22 15:23:08 -0700

    Organizing the repository structure.

--- 20130820 backup.example.sh
+++
@@ -1,9 +0,0 @@
-Это для случая испльзования почтовой программы mailx.
-
-#!/bin/bash
-rsync --delete --log-file=/path/to/$(date +%Y%m%d)_logfile.log -avzq /path/to/source/directory/ /path/to/backup/directory/
-cat /path/to/$(date +%&%m%d)_logfile.log | mail -s “subject line here” youremail@domain.com
-
-
-или в случае использования почтовой программы mutt
-cat /path/to/$(date +%Y%m%d)_logfile.log | mutt -s “subject line here” youremail@domain.com
